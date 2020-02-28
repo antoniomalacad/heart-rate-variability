35c34b4ccdff4a3455d1e614eb1c26957f1d2e0c
Antonio <antoniomalacad@gmail.com>
2020-02-28 20:39:46 +0900

    python scripts

--- index.py
+++ index.py
@@ -6,23 +6,40 @@ from hrv import HRV
 
 fs = 360
 detectors = Detectors(fs)
-rawcsv = np.loadtxt(open("212.csv", "rb"), dtype=int,
-                    delimiter=",", skiprows=1)
+rawcsv212 = np.loadtxt(open("212.csv", "rb"), dtype=int,
+                       delimiter=",", skiprows=1)
+
+rawcsv113 = np.loadtxt(open("113.csv", "rb"), dtype=int,
+                       delimiter=",", skiprows=1)
+
 print("Finished reading csv")
 
-ecg = rawcsv[:, 1]  # ecg only
-r_peaks = detectors.pan_tompkins_detector(ecg)
+ecg212 = rawcsv212[:, 1]
+ecg113 = rawcsv113[:, 1]
+r_peaks212 = detectors.pan_tompkins_detector(ecg212)
+r_peaks113 = detectors.pan_tompkins_detector(ecg113)
 
 myHRV = HRV(360)
-rmssd = myHRV.RMSSD(r_peaks)
-sdnn = myHRV.SDNN(r_peaks)
+rmssd212 = myHRV.RMSSD(r_peaks212)
+sdnn212 = myHRV.SDNN(r_peaks212)
+rmssd113 = myHRV.RMSSD(r_peaks113)
+sdnn113 = myHRV.SDNN(r_peaks113)
 
-dist = list()
-for i in range(1, len(r_peaks), 1):
-    dist.append((r_peaks[i] - r_peaks[i-1]) * 2.76923077)
+rr212 = list()
+for i in range(1, len(r_peaks212), 1):
+    rr212.append((r_peaks212[i] - r_peaks212[i-1]) * 2.76923077)
 
-# plt.hist(dist, bins=25)
+rr113 = list()
+for i in range(1, len(r_peaks113), 1):
+    rr113.append((r_peaks113[i] - r_peaks113[i-1]) * 2.76923077)
 
-plt.plot(ecg[r_peaks[0]:r_peaks[10]])
-plt.title('ECG Sample')
+print("212", np.mean(rr212), rmssd212, sdnn212)
+print("113", np.mean(rr113), rmssd113, sdnn113)
+
+f1 = plt.figure(1)
+plt.plot(ecg212[0:10800])
+plt.suptitle('ECG Sample 212')
+f2 = plt.figure(2)
+plt.plot(ecg113[0:10800])
+plt.suptitle('ECG Sample 113')
 plt.show()
